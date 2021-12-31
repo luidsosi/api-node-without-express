da22255db9e29c40f4c6b81e2e044db565ee704f
Luid Sousa <luidsosi@gmail.com>
2021-12-31 16:29:53 -0300

    crud users and list products

---
+++ src/mocks/products.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 2,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 4,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 6,
+  },
+]
